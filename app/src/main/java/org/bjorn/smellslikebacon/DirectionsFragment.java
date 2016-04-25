536b1da62ddb5b3d2309de5e77bdcc246cf17e93
Bjorn Chambless <bjorn@builtlight.org>
2016-04-25 13:15:35 -0700

    Directions checkboxes

--- app/src/main/java/org/bjorn/smellslikebacon/DirectionsFragment.java
+++ app/src/main/java/org/bjorn/smellslikebacon/DirectionsFragment.java
@@ -1,11 +1,13 @@
 package org.bjorn.smellslikebacon;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,6 +15,9 @@ import android.widget.TextView;
  * Created by bjorn on 4/22/16.
  */
 public class DirectionsFragment extends Fragment{
+    private static final String KEY_CHECKED_BOXES = "checked_boxes";
+    private CheckBox[] mCheckBoxes;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,16 +26,47 @@ public class DirectionsFragment extends Fragment{
 
         LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.directionsLayout);
         String[] directions = Recipes.directions[index].split("`");
-        displayDirections(directions,linearLayout);
+        mCheckBoxes = new CheckBox[directions.length];
+        boolean[] checkedBoxes = new boolean[mCheckBoxes.length];
+        if (savedInstanceState != null && savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES) != null){
+            checkedBoxes = savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES);
+        }
+
+        displayDirectionsCheckboxes(directions,linearLayout,checkedBoxes);
 
         return view;
     }
 
-    private void displayDirections(String[] directions, ViewGroup container){
+    private void displayDirectionsCheckboxes(String[] directions, ViewGroup container, boolean[] checkedBoxes){
+        int i = 0;
         for (String step : directions){
-            TextView textView = new TextView(getActivity());
+            mCheckBoxes[i] = new CheckBox(getActivity());
+            mCheckBoxes[i].setBackgroundColor(Color.WHITE);
+            mCheckBoxes[i].setTextColor(Color.BLACK);
+            mCheckBoxes[i].setPadding(8,16,8,16);
+            mCheckBoxes[i].setTextSize(20f);
+            mCheckBoxes[i].setText("      " + step);
+            container.addView(mCheckBoxes[i]);
+            if (checkedBoxes[i]){
+                mCheckBoxes[i].toggle();
+            }
+            i++;
+
+            /*TextView textView = new TextView(getActivity());
             textView.setText("- "+step);
-            container.addView(textView);
+            container.addView(textView); */
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        boolean[] stateOfCheckBoxes = new boolean[mCheckBoxes.length];
+        int i = 0;
+        for (CheckBox checkBox : mCheckBoxes){
+            stateOfCheckBoxes[i] = checkBox.isChecked();
+            i++;
+        }
+        outState.putBooleanArray(KEY_CHECKED_BOXES,stateOfCheckBoxes);
+        super.onSaveInstanceState(outState);
     }
 }
